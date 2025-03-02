9624cc0c816280e46f3030a808756bcfca67fd00
Hyun-je <bigae2@gmail.com>
2025-03-02 18:57:57 +0900

    create base xcode project

---
+++ LLM_Camera/LLM_CameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  LLM_CameraApp.swift
+//  LLM_Camera
+//
+//  Created by Jaehyeon Park on 3/2/25.
+//
+
+import SwiftUI
+
+@main
+struct LLM_CameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
